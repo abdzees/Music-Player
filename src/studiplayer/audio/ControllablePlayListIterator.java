0f0f4d95094329ccb2f374298975575189a85a55
abdzees <109596800+abdzees@users.noreply.github.com>
2024-05-26 00:19:49 +0200

    Update ControllablePlayListIterator.java
    updated the controllable iterator

--- src/studiplayer/audio/ControllablePlayListIterator.java
+++ src/studiplayer/audio/ControllablePlayListIterator.java
@@ -1,85 +1,88 @@
 package studiplayer.audio;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class ControllablePlayListIterator implements Iterator<AudioFile> {
 
-	private List<AudioFile> list;
-	private int currentLine;
+    private List<AudioFile> list;
+    private int currentLine;
 
-	public ControllablePlayListIterator(List<AudioFile> list) {
-		this.list = new LinkedList<>(list);
-		this.currentLine = 0;
-	}
+    public ControllablePlayListIterator(List<AudioFile> list) {
+        this.list = new LinkedList<>(list);
+        currentLine = 0;
+    }
 
-	public ControllablePlayListIterator(List<AudioFile> list, String search, SortCriterion sortCriterion) {
-		this.list = new LinkedList<>();
-		this.currentLine = 0;
+    public ControllablePlayListIterator(List<AudioFile> list, String search, SortCriterion sortCriterion) {
+        this.list = new LinkedList<>();
+        currentLine = 0;
 
-		// Filter the list based on the search parameter if it's not null or empty
-		if (search == null || search.isEmpty()) {
-			this.list.addAll(list);
-		} else {
-			for (AudioFile file : list) {
-				if (file.getAuthor() != null && file.getAuthor().contains(search)
-					|| (file.getTitle() != null && file.getTitle().contains(search))
-					|| (file instanceof TaggedFile && ((TaggedFile) file).getAlbum() != null
-							&& ((TaggedFile) file).getAlbum().contains(search))) {
-					this.list.add(file);
-				}
-			}
-		}
+        // Filter the list based on the search parameter if it's not null or empty
+        if (search == null || search.isEmpty()) {
+            this.list.addAll(list);
+        } else {
+            for (AudioFile file : list) {
+                if ((file.getAuthor() != null && file.getAuthor().toLowerCase().contains(search.toLowerCase()))
+                        || (file.getTitle() != null && file.getTitle().toLowerCase().contains(search.toLowerCase()))
+                        || (file instanceof TaggedFile && ((TaggedFile) file).getAlbum() != null
+                                && ((TaggedFile) file).getAlbum().toLowerCase().contains(search.toLowerCase()))) {
+                    this.list.add(file);
+                }
+            }
+        }
 
-		// Sort the list based on the sort parameter
-		switch (sortCriterion) {
-		case AUTHOR:
-			Collections.sort(this.list, new AuthorComparator());
-			break;
-		case TITLE:
-			Collections.sort(this.list, new TitleComparator());
-			break;
-		case ALBUM:
-			Collections.sort(this.list, new AlbumComparator());
-			break;
-		case DURATION:
-			Collections.sort(this.list, new DurationComparator());
-			break;
-		default:
-			break; // No sorting needed for DEFAULT
+        // Sort the list according to the sort criterion
+        switch (sortCriterion) {
+            case AUTHOR:
+                this.list.sort(new AuthorComparator());
+                break;
+            case ALBUM:
+                this.list.sort(new AlbumComparator());
+                break;
+            case TITLE:
+                this.list.sort(new TitleComparator());
+                break;
+            case DURATION:
+                this.list.sort(new DurationComparator());
+                break;
+            default:
+                break;
+        }
+    }
 
-		}
-	}
+    @Override
+    public boolean hasNext() {
+        return currentLine < list.size();
+    }
 
-	@Override
-	public boolean hasNext() {
-		return this.currentLine < list.size();
+    @Override
+    public AudioFile next() {
+        if (!hasNext()) {
+        	currentLine = 0;
+        	return list.get(currentLine++);
+        }
+        return list.get(currentLine++);
+    }
 
-	}
-
-	@Override
-	public AudioFile next() {
-		if (hasNext()) {
-			return list.get(currentLine++);
-		} else {
-			throw new NoSuchElementException("No more elements in the playlist");
-			
-		}
-	}
-
-	public AudioFile jumpToAudioFile(AudioFile file) {
-	    int index = list.indexOf(file); // Find the index of the AudioFile
-	    if (index != -1) { // The AudioFile exists
-	        currentLine = index + 1; // Update the currentIndex so that the next call to next() returns the song after the given AudioFile
-	        return file;
-	    } else {
-	        return null; // AudioFile not found in the list
-	    }
-	}
+    public AudioFile jumpToAudioFile(AudioFile file) {
+        if (list.contains(file)) {
+            currentLine = list.indexOf(file);
+            return list.get(currentLine++);
+        } else {
+            return null;
+        }
+    }
 
 
+    public int getCurrentLine() {
+        return currentLine;
+    }
 
+    public void setCurrentLine(int index) {
+        this.currentLine = index;
+    }
+
+    public List<AudioFile> getList() {
+        return this.list;
+    }
+    
 }
