623128a5a074b0ece5ee7793a22c349d9694e2f0
abdzees <109596800+abdzees@users.noreply.github.com>
2024-05-26 00:18:25 +0200

    Update AlbumComparator.java
    handled files with no album

--- src/studiplayer/audio/AlbumComparator.java
+++ src/studiplayer/audio/AlbumComparator.java
@@ -1,33 +1,42 @@
 package studiplayer.audio;
-
 import java.util.Comparator;
 
-public class AlbumComparator implements Comparator<AudioFile> {
-
-	private String getAlbum(AudioFile file) {
-		if (file instanceof TaggedFile) {
-			return ((TaggedFile) file).getAlbum();
-		}
-		return null;
-	}
+public class AlbumComparator implements Comparator<Object>{
 
 	@Override
-	public int compare(AudioFile o1, AudioFile o2) {
-
-		if (o1 != null && o2 != null) {
-			String album1 = getAlbum(o1);
-			String album2 = getAlbum(o2);
-			if (album1 == null && album2 == null)
-				return 0;
-			if (album1 == null)
-				return -1;
-			if (album2 == null)
-				return 1;
-			return album1.compareTo(album2);
-
-		} else {
+	public int compare(Object o1, Object o2) {
+		if (o1 == null || o2 == null)
 			throw new RuntimeException();
-		}
+		
+		// if not a tag file, rank before other
+		if (!(o1 instanceof TaggedFile) && !(o2 instanceof TaggedFile))
+			return 0;
+		if (!(o1 instanceof TaggedFile))
+			return -1;
+		if (!(o2 instanceof TaggedFile))
+			return 1;
+		
+		TaggedFile tf1 = (TaggedFile) o1;
+		TaggedFile tf2 = (TaggedFile) o2;
+		
+		// check if Album is exists
+		if (tf1.getAlbum() == null && tf2.getAlbum() == null)
+			return 0;
+		if (tf1.getAlbum() == null)
+			return -1;
+		if (tf2.getAlbum() == null)
+			return 1;
+		
+		// check if Album is empty
+		if (tf1.getAlbum().equals("") && tf2.getAlbum().equals(""))
+			return 0;
+		if (tf1.getAlbum().equals(""))
+			return -1;
+		if (tf2.getAlbum().equals(""))
+			return 1;
+		
+		// if album is present, compare
+		return tf1.getAlbum().compareTo(tf2.getAlbum());
 	}
 
 }
